510dd6fbfbc53089d819ca5f2e06482669cdc248
Wassim <Xwassim04@gmail.com>
2025-02-06 13:42:16 -0500

    semaine3.4.4

--- components/EventCard.jsx
+++ components/EventCard.jsx
@@ -1,10 +1,30 @@
 export default function EventCard({ title, description, image }) {
-    return (
-      <div style={{ border: '1px solid #ccc', padding: '10px', margin: '10px 0', borderRadius: '5px', textAlign: 'center' }}>
-        <h2>{title}</h2>
-        <p>{description}</p>
-        <img src={image} alt={title} style={{ maxWidth: '100%', borderRadius: '5px', marginTop: '10px' }} />
+  return (
+    <div
+      style={{
+        border: '1px solid #ccc',
+        padding: '10px',
+        margin: '10px 0',
+        borderRadius: '5px',
+        textAlign: 'center',
+      }}
+    >
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',  
+          marginTop: '10px',
+        }}
+      >
+        <img
+          src={image}
+          alt={title}
+          style={{ maxWidth: '100%', borderRadius: '5px' }}
+        />
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
